365f0043cddbb5549de29c366b5ef3a5d50ee3b3
khlam <4841220+khlam@users.noreply.github.com>
2021-07-16 14:29:20 -0700

    s1 top

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 
+function showText(value) {
+    document.getElementById("featureName").innerText = value
+}
+
 function main() {
-    console.log("here")
     /*
     let allGrids = document.getElementsByClassName("grid").forEach(element => {
         element.style = `height:100px;width:100px`
