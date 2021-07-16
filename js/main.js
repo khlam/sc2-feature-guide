c1ff32c20778ad01225b20ac93a14a559e9ca3fc
khlam <4841220+khlam@users.noreply.github.com>
2021-07-16 13:40:28 -0700

    init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+function main() {
+    console.log("here")
+    /*
+    let allGrids = document.getElementsByClassName("grid").forEach(element => {
+        element.style = `height:100px;width:100px`
+    })*/
+}
+
+window.addEventListener('load', (event) => {
+    main()
+})
